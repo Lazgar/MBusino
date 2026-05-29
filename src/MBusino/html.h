8577d883bbbc21b8fe01940d098a282fcf3af14b
Lazgar <34341913+Lazgar@users.noreply.github.com>
2026-05-29 07:58:26 +0200

    Update html.h

--- src/MBusino/html.h
+++ src/MBusino/html.h
@@ -80,8 +80,8 @@ const char index_html[] PROGMEM = R"rawliteral(
         <div class='form-floating'><label>Sensor publish interval sec.</label><input type='text' value='%u' class='form-control' name='sensorInterval'></div>
         <div class='form-floating'><label>M-Bus publish interval sec.</label><input type='text' value='%u' class='form-control' name='mbusInterval'></div>
 
-		<div class='form-floating'><label>M-Bus Daten entschlüsseln (OMS Mode 5):</label><input type="checkbox" id="decrypt_enabled" name="decrypt_enabled" %DECRYPT_CHECKED%>
-		<div class='form-floating'><label for="aes_key">AES-128 Key (32-stelliger Hex-Code):</label><input type="text" id="aes_key" name="aes_key" value="%AES_KEY_VAL%" maxlength="32" placeholder="00112233445566778899AABBCCDDEEFF">
+		<div class='form-floating'><label>M-Bus Daten entschlüsseln (OMS Mode 5):</label><input type='checkbox' class='form-control' name='decrypt_enabled'></div>
+		<div class='form-floating'><label>AES-128 Key (32-stelliger Hex-Code):</label><input type='text' class='form-control' value='%s' maxlength='32' name='aes_key'>></div>
 
         <div class='form-floating'><label>MQTT Broker</label><input type='text' value='%s' class='form-control' maxlength = '64' name='broker'></div>
         <div class='form-floating'><label>MQTT Port</label><input type='text' value='%u' class='form-control' name='mqttPort'></div>
